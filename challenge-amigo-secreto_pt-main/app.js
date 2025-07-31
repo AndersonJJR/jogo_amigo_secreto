74209a755bcf405e168b9dcaab4364649fd79980
AndersonJJR <adasilvachavesjunior@gmail.com>
2025-07-30 21:33:37 -0300

    jogo amigo secreto 1.0

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,50 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaComNomes = [];
+
+//Nesta função é responsavel por gerar um loop de while(enquanto), que vai acumulando nomes em uma lista a partir da entrada do usuario.
+function adicionarAmigo(){
+
+    while (true){
+
+        let entrada = document.querySelector('input').value;
+
+        if (entrada == ''){
+            return prompt('Não há nome digitado!');
+        } else{
+            listaComNomes.push(entrada);
+            limparCampo();
+            return mostrarAmigosNaTela('listaAmigos');   
+        }
+    }
+}
+
+//Nesta função usamos o loop for por ter um número limitados de elementos presentes na lista, geramos um número aleatorio com o comprimento da lista
+//assim pegando um index aleatorio sendo o sorteado.
+function sortearAmigo(){
+
+    for (amigo in listaComNomes){
+        
+        let indexSorteado = parseInt(Math.random() * listaComNomes.length)
+        let sorteado = listaComNomes[indexSorteado]
+
+        let mostrarResultado = document.getElementById('resultado');
+        mostrarResultado.innerHTML = '';
+        return mostrarResultado.append(sorteado)
+    }
+}
+
+function mostrarAmigosNaTela(tag){
+    let textoTela = document.getElementById(tag);
+
+        for (amigo in listaComNomes){
+            let li = document.createElement('li');
+            textoTela.innerHTML = '';
+            li.textContent = listaComNomes;
+            textoTela.appendChild(li);
+        }
+}
+
+function limparCampo(){
+    campoDigitado = document.getElementById('amigo');
+    campoDigitado.value = '';
+}
